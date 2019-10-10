c2970892990ab3b0fc373a1f24c3e50cba4e112d
Leslie Torres <leslietorres@MacBook-Air-de-Leslie.local>
2019-10-10 16:16:36 -0500

    project

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  
+</template>
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+  name: "Navbar",
+  data: () => ({ 
+    close: false
+  }),
+
+  methods: {
+   
+  },
+
+  computed:{
+    
+    
+  }
+};
+</script>
+<style scoped>
+.nav-bar-style {
+  background-color: #08799c;
+}
+</style>
